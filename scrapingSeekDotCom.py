26821dc70d54772afcd474e424a1d62dbbc2b234
Kevin Sequeira <kevinsequeira@192-168-1-121.tpgi.com.au>
2020-11-07 19:31:02 +1100

    Ninth Commit

--- scrapingSeekDotCom.py
+++ scrapingSeekDotCom.py
@@ -9,6 +9,10 @@ import json
 from datetime import datetime, timedelta
 import pandas as pan
 
+import os
+print("Working Directory:", os.getcwd())
+os.chdir(os.getcwd())
+
 ## ========================================================================== ##
 ## Initialize all the variables necessary for the data scraping process.      ##
 ## ========================================================================== ##
@@ -21,13 +25,15 @@ def initializeVariables():
     jobSearchStrings = {"Data Science": ["\"Senior Data Scientist\"", "\"Data Scientist\""],
                         "Data Analysis": ["\"Senior Data Analyst\"", "\"Data Analyst\"", "\"Graduate Data Analyst\""],
                         "Data Engineering": ["\"Senior Data Engineer\"", "\"Data Engineer\""],
-                        "Data Architect": ["\"Data Architect\""],
-                        "Business Intelligence": ["\"Business Intelligence Developer\"", "\"BI Developer\"", "\"Business Intelligence Specialist\"", "\"BI Specialist\"", "\"Business Intelligence Analyst\"", "\"BI Analyst\""],
-                        "Machine Learning": ["\"Machine Learning Engineer\"", "\"Junior Machine Learning Engineer\"", "\"Graduate Machine Learning Engineer\""],
+                        "Data Architect": ["\"Senior Data Architect\"", "\"Data Architect\""],
+                        "Business Intelligence": ["\"Senior Business Intelligence Developer\"", "\"Senior BI Developer\"", "\"Senior Business Intelligence Specialist\"", "\"Senior BI Specialist\"", "\"Senior Business Intelligence Analyst\"", "\"Senior BI Analyst\"",
+                            "\"Business Intelligence Developer\"", "\"BI Developer\"", "\"Business Intelligence Specialist\"", "\"BI Specialist\"", "\"Business Intelligence Analyst\"", "\"BI Analyst\""],
+                        "Machine Learning": ["\"Senior Machine Learning Engineer\"", "\"Machine Learning Engineer\""],
                         "Data Visualization": ["\"Data Visualization Developer\""],
-                        "ETL Development": ["\"ETL Developer\"", "\"Junior ETL Developer\"", "\"Graduate ETL Developer\""],
+                        "ETL Development": ["\"Senior ETL Developer\"" "\"ETL Developer\""],
                         "Database Administration": ["\"Senior Database Developer\"", "\"Database Developer\"", "\"Senior Database Administrator\"", "\"Database Administrator\""],
-                        "Cloud Development": ["\"Cloud Developer\"", "\"Cloud Engineer\""]}
+                        "Cloud Development": ["\"Senior Cloud Developer\"", "\"Senior Cloud Engineer\"", "\"Senior Cloud Architect\"", "\"Senior Cloud Consultant\"",
+                            "\"Cloud Developer\"", "\"Cloud Engineer\"", "\"Cloud Architect\"", "\"Cloud Consultant\""]}
     return jobSearchStrings
 
 def getLatestJobs():
@@ -50,21 +56,83 @@ def getJobDetails(article, latestDate: datetime, domain: str, searchString: str,
     listingDate = datetime.strptime(" ".join(json.loads(jsonText)["jobdetails"]["result"]["listingDate"].split("T"))[:-5], "%Y-%m-%d %H:%M:%S")
     # print(date001.strftime('%w'))
     if (latestDate < listingDate):
-        print(listingDate)
         return None
     if getFullDetails == "no":
         return {"job-id": article["data-job-id"],
                 "job-domain": domain,
-                "search-string": searchString,
-                "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip(),
-                "job-listing-date": " ".join(json.loads(jsonText)["jobdetails"]["result"]["listingDate"].split("T"))[:-5],
-                "job-url": "https://www.seek.com.au/job/" + article["data-job-id"]}
+                "search-string": searchString.replace("\"", ""),
+                "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip()}
     elif getFullDetails == "yes":
         # print(searchString, article["data-job-id"], json.loads(jsonText)["jobdetails"]["result"]["title"].strip())
-        return {"job-id": article["data-job-id"],
-                "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip(),
-                "job-listing-date": " ".join(json.loads(jsonText)["jobdetails"]["result"]["listingDate"].split("T"))[:-5],
-                "job-url": "https://www.seek.com.au/job/" + article["data-job-id"]}
+        jobAdvertiser = json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["description"]
+        if "searchParams" in json.loads(jsonText)["jobdetails"]["result"]["advertiser"].keys():
+            if "keywords" in json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["searchParams"].keys():
+                jobAdvertiser = json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["searchParams"]["keywords"]
+        else:
+            jobAdvertiser = json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["description"]
+        jobAdvertiserEmail = ""
+        jobAdvertiserPhone = ""
+        contactsList = []
+        if "contactMatches" in json.loads(jsonText)["jobdetails"]["result"].keys():
+            if len(json.loads(jsonText)["jobdetails"]["result"]["contactMatches"]) > 0:
+                for dictionary in json.loads(jsonText)["jobdetails"]["result"]["contactMatches"]:
+                    if dictionary["type"] == "Email":
+                        jobAdvertiserEmail = "Yes"
+                        emailAddress = dictionary["value"].split(";")[-1].lower()
+                        contactsList.append([article["data-job-id"], "Email", emailAddress])
+                    elif dictionary["type"] == "Phone":
+                        phoneNumber = dictionary["value"].replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+                        if (len(phoneNumber) == 8):
+                            if (json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["state"].lower() in ["new south wales", "australian capital territory"]):
+                                phoneNumber = "+61 2" + phoneNumber
+                            elif (json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["state"].lower() in ["victoria", "tasmania"]):
+                                phoneNumber = "+61 3" + phoneNumber
+                            elif (json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["state"].lower() in ["queensland"], ):
+                                phoneNumber = "+61 3" + phoneNumber
+                            elif (json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["state"].lower() in ["western australia", "south australia", "nothern territory"], ):
+                                phoneNumber = "+61 3" + phoneNumber
+                        elif (len(phoneNumber) == 9) & (phoneNumber[0] in ("2", "3", "7", "8")):
+                            phoneNumber = "+61 " + phoneNumber
+                        elif (len(phoneNumber) == 10) & (phoneNumber[0] == "0"):
+                            phoneNumber = "+61 " + phoneNumber[1:]
+                        elif (len(phoneNumber) == 11) & (phoneNumber[0:2] == "61"):
+                            phoneNumber = "+61 " + phoneNumber[2:]
+                        elif (phoneNumber[0:3] == "+61"):
+                            phoneNumber = phoneNumber.replace("+610", "+61").replace("+61", "+61 ")
+                        if len(phoneNumber) == 8:
+                            continue
+                        print(article["data-job-id"], phoneNumber)
+                        jobAdvertiserPhone = "Yes"
+                        contactsList.append([article["data-job-id"], "Phone", phoneNumber])
+        citizenshipMentioned = ""
+        if "desktopAdTemplate" in json.loads(jsonText)["jobdetails"]["result"].keys():
+            adTemplate = "desktopAdTemplate"
+        elif "mobileAdTemplate" in json.loads(jsonText)["jobdetails"]["result"].keys():
+            adTemplate = "mobileAdTemplate"
+        if "citizen" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower():
+            if ("australian citizen" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()) |\
+            ("nz citizen" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()) |\
+            ("citizenship" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()):
+                citizenshipMentioned = "Yes"
+        elif "clearance" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower():
+            if ("security clearance" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()) |\
+            ("baseline security clearance" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()) |\
+            ("baseline clearance" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()) |\
+            ("nv1" in json.loads(jsonText)["jobdetails"]["result"][adTemplate].lower()):
+                citizenshipMentioned = "Yes"
+        jobDetails = {"job-id": article["data-job-id"],
+            "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip(),
+            "job-listing-date": " ".join(json.loads(jsonText)["jobdetails"]["result"]["listingDate"].split("T"))[:-5],
+            "job-url": "https://www.seek.com.au/job/" + article["data-job-id"],
+            "job-location-state": json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["state"],
+            "job-location-city": json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["city"],
+            "job-location-area": json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["area"],
+            "job-location-suburb": json.loads(jsonText)["jobdetails"]["result"]["locationHierarchy"]["suburb"],
+            "job-citizenship-required": citizenshipMentioned,
+            "job-advertiser": jobAdvertiser,
+            "job-advertiser-email": jobAdvertiserEmail,
+            "job-advertiser-phone": jobAdvertiserPhone}
+        return jobDetails, contactsList
 
 def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jobsIDList: list = []):
     """
@@ -86,6 +154,7 @@ def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jo
     searchStringURL = "%20".join([text for text in searchString.split(" ")])
     jobsList = []
     jobsMasterList = []
+    jobsContactsList = []
     pageCount = 1
     stopIteration = "No"
     while True:
@@ -109,17 +178,19 @@ def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jo
             if jobDict is not None:
                 jobsList.append(jobDict)
                 if jobDict["job-id"] not in jobsIDList:
-                    jobsMasterList.append(getJobDetails(article = article,
+                    jobDetails, contactsList = getJobDetails(article = article,
                                             latestDate = latestDate,
                                             domain = domain,
                                             searchString = searchString,
-                                            getFullDetails = "yes"))
+                                            getFullDetails = "yes")
+                    jobsMasterList.append(jobDetails)
                     jobsIDList.append(jobDict["job-id"])
+                    jobsContactsList = jobsContactsList + contactsList
         if stopIteration == "Yes":
             break
         pageCount += 1
 
-    return pan.DataFrame(jobsList), pan.DataFrame(jobsMasterList), jobsIDList
+    return pan.DataFrame(jobsList), pan.DataFrame(jobsMasterList), jobsIDList, jobsContactsList
 
 def loopOverRoles(latestDate: datetime, scrapedJobs: pan.DataFrame, scrapedJobsMaster: pan.DataFrame, jobRoles: dict):
     """
@@ -141,29 +212,34 @@ def loopOverRoles(latestDate: datetime, scrapedJobs: pan.DataFrame, scrapedJobsM
     jobsIDMasterList -> list
     """
     jobsIDMasterList = []
+    contactsList = []
     for domain, searchStrings in jobRoles.items():
         for searchString in searchStrings:
             print("Domain:", domain + ",", "Search String:", searchString)
             print("Number of jobs:", len(jobsIDMasterList))
-            jobsDataFrame, jobsMasterDataFrame, jobsIDList = scrapeSearchResults(latestDate = latestDate,
+            jobsDataFrame, jobsMasterDataFrame, jobsIDList, jobsContactsList = scrapeSearchResults(latestDate = latestDate,
                 domain = domain,
                 searchString = searchString,
                 jobsIDList = jobsIDMasterList)
             scrapedJobs = scrapedJobs.append(jobsDataFrame)
             scrapedJobsMaster = scrapedJobsMaster.append(jobsMasterDataFrame)
             jobsIDMasterList = jobsIDMasterList
+            contactsList = contactsList + jobsContactsList
             print("Number of jobs:", len(jobsIDMasterList))
 
-    return scrapedJobs, scrapedJobsMaster
+    return scrapedJobs, scrapedJobsMaster, contactsList
 
 if __name__ == '__main__':
-    scrapedJobs = pan.DataFrame(columns=["job-id", "job-domain", "search-string",
-                                         "job-title", "job-listing-date", "job-url"])
-    scrapedJobsMaster = pan.DataFrame(columns=["job-id", "job-title", "job-listing-date", "job-url"])
+    scrapedJobs = pan.DataFrame(columns=["job-id", "job-domain", "search-string", "job-title"])
+    scrapedJobsMaster = pan.DataFrame(columns=["job-id", "job-title", "job-listing-date", "job-url",
+                                               "job-location-state", "job-location-city", "job-location-area",
+                                               "job-location-suburb", "job-advertiser", "job-advertiser-email", "job-advertiser-phone"])
     jobSearchStrings = initializeVariables()
-    dataJobsDataframe, dataJobsMasterDataFrame = loopOverRoles(latestDate = datetime(2020, 11, 5, 23, 59, 59, 99999),
+    dataJobsDataframe, dataJobsMasterDataFrame, contactsList = loopOverRoles(latestDate = datetime(2020, 11, 6, 23, 59, 59, 99999),
         scrapedJobs = scrapedJobs,
         scrapedJobsMaster = scrapedJobsMaster,
         jobRoles = jobSearchStrings)
     dataJobsDataframe.to_csv("Seek Data Jobs.csv", index = False)
     dataJobsMasterDataFrame.to_csv("Seek Data Jobs Master.csv", index = False)
+    dataFrameJobContacts = pan.DataFrame(contactsList, columns = ["job-id", "contact-type", "value"])
+    dataFrameJobContacts.to_csv("Seek Data Jobs Contacts.csv", index = False)
--- testJobScrape.py
+++ testJobScrape.py
@@ -11,13 +11,21 @@ import json
 from datetime import datetime, timedelta
 import pandas as pan
 
-jobPost = requests.get("https://www.seek.com.au/job/50770272").text
+jobPost = requests.get("https://www.seek.com.au/job/50786699").text
 jobSoupObject = BeautifulSoup(jobPost, "html.parser")
 scriptWithPostIDs = jobSoupObject.find("script", text = re.compile("window.SEEK_REDUX_DATA"))
 jsonText = re.search(r'^\s*window\.SEEK_REDUX_DATA\s*=\s*({.*?})\s*;\s*$',
               scriptWithPostIDs.string, flags = re.DOTALL | re.MULTILINE).group(1)
 jsonText = jsonText.replace(': null', ': "null"').replace(': false', ': false"').replace(': true', ': "true"').replace('undefined', '"undefined"')
-print(json.loads(jsonText)["jobdetails"]["result"])
+if "citizen" in json.loads(jsonText)["jobdetails"]["result"]["desktopAdTemplate"].lower():
+    if ("australian citizen" in json.loads(jsonText)["jobdetails"]["result"]["desktopAdTemplate"].lower()) |\
+    ("nz citizen" in json.loads(jsonText)["jobdetails"]["result"]["desktopAdTemplate"].lower()) |\
+    ("citizenship" in json.loads(jsonText)["jobdetails"]["result"]["desktopAdTemplate"].lower()):
+        print("citizenship-required:", "yes")
+    else:
+        print("citizenship-required:", "no")
+else:
+    print("citizenship-required:", "no")
 print("job-id:", json.loads(jsonText)["jobdetails"]["result"]["id"])
 print("job-title:", json.loads(jsonText)["jobdetails"]["result"]["title"])
 print("job-advertiser:", json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["searchParams"]["keywords"])
@@ -28,3 +36,9 @@ print("job-location-suburb:", json.loads(jsonText)["jobdetails"]["result"]["loca
 print("job-classification:", json.loads(jsonText)["jobdetails"]["result"]["classification"]["description"])
 print("job-sub-classification:", json.loads(jsonText)["jobdetails"]["result"]["subClassification"]["description"])
 print("job-role-titles:", json.loads(jsonText)["jobdetails"]["result"]["roleTitles"])
+if len(json.loads(jsonText)["jobdetails"]["result"]["contactMatches"]) > 0:
+    for dictionary in json.loads(jsonText)["jobdetails"]["result"]["contactMatches"]:
+        if dictionary["type"] == "Email":
+            print("job-advertiser-email-available:", dictionary["value"])
+        elif dictionary["type"] == "Phone":
+            print("job-advertiser-mobile-available:", dictionary["value"])
