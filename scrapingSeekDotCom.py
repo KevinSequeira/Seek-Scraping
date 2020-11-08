392304156cc9ea823f17e7b876f027165c96ee26
Kevin Sequeira <kevinsequeira@192-168-1-121.tpgi.com.au>
2020-11-08 11:00:34 +1100

    Tenth Commit

--- scrapingSeekDotCom.py
+++ scrapingSeekDotCom.py
@@ -22,18 +22,18 @@ os.chdir(os.getcwd())
 ## ========================================================================== ##
 def initializeVariables():
     ## Initialize a dictionary of all relevant job search strings.
-    jobSearchStrings = {"Data Science": ["\"Senior Data Scientist\"", "\"Data Scientist\""],
-                        "Data Analysis": ["\"Senior Data Analyst\"", "\"Data Analyst\"", "\"Graduate Data Analyst\""],
-                        "Data Engineering": ["\"Senior Data Engineer\"", "\"Data Engineer\""],
-                        "Data Architect": ["\"Senior Data Architect\"", "\"Data Architect\""],
-                        "Business Intelligence": ["\"Senior Business Intelligence Developer\"", "\"Senior BI Developer\"", "\"Senior Business Intelligence Specialist\"", "\"Senior BI Specialist\"", "\"Senior Business Intelligence Analyst\"", "\"Senior BI Analyst\"",
-                            "\"Business Intelligence Developer\"", "\"BI Developer\"", "\"Business Intelligence Specialist\"", "\"BI Specialist\"", "\"Business Intelligence Analyst\"", "\"BI Analyst\""],
-                        "Machine Learning": ["\"Senior Machine Learning Engineer\"", "\"Machine Learning Engineer\""],
-                        "Data Visualization": ["\"Data Visualization Developer\""],
-                        "ETL Development": ["\"Senior ETL Developer\"" "\"ETL Developer\""],
-                        "Database Administration": ["\"Senior Database Developer\"", "\"Database Developer\"", "\"Senior Database Administrator\"", "\"Database Administrator\""],
-                        "Cloud Development": ["\"Senior Cloud Developer\"", "\"Senior Cloud Engineer\"", "\"Senior Cloud Architect\"", "\"Senior Cloud Consultant\"",
-                            "\"Cloud Developer\"", "\"Cloud Engineer\"", "\"Cloud Architect\"", "\"Cloud Consultant\""]}
+    jobSearchStrings = {"Data Science": ["\"Senior Data Scientist\"", "\"Data Scientist\""],}
+                        # "Data Analysis": ["\"Senior Data Analyst\"", "\"Data Analyst\"", "\"Graduate Data Analyst\""],
+                        # "Data Engineering": ["\"Senior Data Engineer\"", "\"Data Engineer\""],
+                        # "Data Architect": ["\"Senior Data Architect\"", "\"Data Architect\""],
+                        # "Business Intelligence": ["\"Senior Business Intelligence Developer\"", "\"Senior BI Developer\"", "\"Senior Business Intelligence Specialist\"", "\"Senior BI Specialist\"", "\"Senior Business Intelligence Analyst\"", "\"Senior BI Analyst\"",
+                        #     "\"Business Intelligence Developer\"", "\"BI Developer\"", "\"Business Intelligence Specialist\"", "\"BI Specialist\"", "\"Business Intelligence Analyst\"", "\"BI Analyst\""],
+                        # "Machine Learning": ["\"Senior Machine Learning Engineer\"", "\"Machine Learning Engineer\""],
+                        # "Data Visualization": ["\"Data Visualization Developer\""],
+                        # "ETL Development": ["\"Senior ETL Developer\"", "\"ETL Developer\""],
+                        # "Database Administration": ["\"Senior Database Developer\"", "\"Database Developer\"", "\"Senior Database Administrator\"", "\"Database Administrator\""],
+                        # "Cloud Development": ["\"Senior Cloud Developer\"", "\"Senior Cloud Engineer\"", "\"Senior Cloud Architect\"", "\"Senior Cloud Consultant\"",
+                        #     "\"Cloud Developer\"", "\"Cloud Engineer\"", "\"Cloud Architect\"", "\"Cloud Consultant\""]}
     return jobSearchStrings
 
 def getLatestJobs():
@@ -61,7 +61,8 @@ def getJobDetails(article, latestDate: datetime, domain: str, searchString: str,
         return {"job-id": article["data-job-id"],
                 "job-domain": domain,
                 "search-string": searchString.replace("\"", ""),
-                "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip()}
+                "job-title": json.loads(jsonText)["jobdetails"]["result"]["title"].strip(),
+                "job-listing-date": " ".join(json.loads(jsonText)["jobdetails"]["result"]["listingDate"].split("T"))[:-5]}
     elif getFullDetails == "yes":
         # print(searchString, article["data-job-id"], json.loads(jsonText)["jobdetails"]["result"]["title"].strip())
         jobAdvertiser = json.loads(jsonText)["jobdetails"]["result"]["advertiser"]["description"]
@@ -101,7 +102,7 @@ def getJobDetails(article, latestDate: datetime, domain: str, searchString: str,
                             phoneNumber = phoneNumber.replace("+610", "+61").replace("+61", "+61 ")
                         if len(phoneNumber) == 8:
                             continue
-                        print(article["data-job-id"], phoneNumber)
+                        # print(article["data-job-id"], phoneNumber)
                         jobAdvertiserPhone = "Yes"
                         contactsList.append([article["data-job-id"], "Phone", phoneNumber])
         citizenshipMentioned = ""
@@ -131,10 +132,12 @@ def getJobDetails(article, latestDate: datetime, domain: str, searchString: str,
             "job-citizenship-required": citizenshipMentioned,
             "job-advertiser": jobAdvertiser,
             "job-advertiser-email": jobAdvertiserEmail,
-            "job-advertiser-phone": jobAdvertiserPhone}
+            "job-advertiser-phone": jobAdvertiserPhone,
+            "job-classification": json.loads(jsonText)["jobdetails"]["result"]["classification"]["description"],
+            "job-sub-classification": json.loads(jsonText)["jobdetails"]["result"]["subClassification"]["description"]}
         return jobDetails, contactsList
 
-def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jobsIDList: list = []):
+def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, latestTimestamp: str, jobsIDList: list = []):
     """
     This function takes in three parameters, the latest date up to which jobs
     are to be scraped, and the search string to be entered in the search bar.
@@ -156,8 +159,8 @@ def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jo
     jobsMasterList = []
     jobsContactsList = []
     pageCount = 1
-    stopIteration = "No"
-    while True:
+    continueSearch = True
+    while continueSearch:
         jobPosts= requests.get("https://www.seek.com.au/jobs?classification=1209%2C1210%2C6281%2C1223&keywords=" + searchStringURL + "&page=" + str(pageCount) + "&sortmode=ListedDate").text
         soupObject = BeautifulSoup(jobPosts, "html.parser")
         allPageArticles = soupObject.find("div", {"data-automation": "searchResults"})
@@ -175,6 +178,9 @@ def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jo
                                     domain = domain,
                                     searchString = searchString,
                                     getFullDetails = "no")
+            if jobDict["job-listing-date"] <= latestTimestamp:
+                continueSearch = False
+                break
             if jobDict is not None:
                 jobsList.append(jobDict)
                 if jobDict["job-id"] not in jobsIDList:
@@ -183,16 +189,15 @@ def scrapeSearchResults(latestDate: datetime, domain: str, searchString: str, jo
                                             domain = domain,
                                             searchString = searchString,
                                             getFullDetails = "yes")
+                    print(jobDict["job-id"], jobDict["job-listing-date"], jobDict["job-title"])
                     jobsMasterList.append(jobDetails)
                     jobsIDList.append(jobDict["job-id"])
                     jobsContactsList = jobsContactsList + contactsList
-        if stopIteration == "Yes":
-            break
         pageCount += 1
 
     return pan.DataFrame(jobsList), pan.DataFrame(jobsMasterList), jobsIDList, jobsContactsList
 
-def loopOverRoles(latestDate: datetime, scrapedJobs: pan.DataFrame, scrapedJobsMaster: pan.DataFrame, jobRoles: dict):
+def loopOverRoles(latestDate: datetime, scrapedJobs: pan.DataFrame, scrapedJobsMaster: pan.DataFrame, jobRoles: dict, latestTimestamps: pan.DataFrame):
     """
     This function loops over different roles and appends all the search results
     into one dataset. The function takes in three parameters, the latest date up
@@ -215,31 +220,85 @@ def loopOverRoles(latestDate: datetime, scrapedJobs: pan.DataFrame, scrapedJobsM
     contactsList = []
     for domain, searchStrings in jobRoles.items():
         for searchString in searchStrings:
-            print("Domain:", domain + ",", "Search String:", searchString)
-            print("Number of jobs:", len(jobsIDMasterList))
+            print("Domain:", domain + ",", "Search String:", searchString.replace("\"", ""))
+            print("Number of jobs to start:", len(jobsIDMasterList))
+            latestTimestamp = latestTimestamps["latest-timestamp"][latestTimestamps["search-string"] == searchString.replace("\"", "")].tolist()[0]
             jobsDataFrame, jobsMasterDataFrame, jobsIDList, jobsContactsList = scrapeSearchResults(latestDate = latestDate,
                 domain = domain,
                 searchString = searchString,
+                latestTimestamp = latestTimestamp,
                 jobsIDList = jobsIDMasterList)
             scrapedJobs = scrapedJobs.append(jobsDataFrame)
             scrapedJobsMaster = scrapedJobsMaster.append(jobsMasterDataFrame)
             jobsIDMasterList = jobsIDMasterList
             contactsList = contactsList + jobsContactsList
-            print("Number of jobs:", len(jobsIDMasterList))
+            print("Number of jobs to end:", len(jobsIDMasterList))
 
     return scrapedJobs, scrapedJobsMaster, contactsList
 
 if __name__ == '__main__':
-    scrapedJobs = pan.DataFrame(columns=["job-id", "job-domain", "search-string", "job-title"])
+    scrapedJobs = pan.DataFrame(columns=["job-id", "job-domain", "search-string", "job-title", "job-listing-date"])
     scrapedJobsMaster = pan.DataFrame(columns=["job-id", "job-title", "job-listing-date", "job-url",
                                                "job-location-state", "job-location-city", "job-location-area",
-                                               "job-location-suburb", "job-advertiser", "job-advertiser-email", "job-advertiser-phone"])
+                                               "job-location-suburb", "job-advertiser", "job-advertiser-email", "job-advertiser-phone",
+                                               "job-classification", "job-sub-classification"])
+    try:
+        scrapedJobsLatest = pan.read_csv("Scraped Jobs Latest Timestamps.csv")
+    except:
+        scrapedJobsLatest = pan.DataFrame([["Senior Data Scientist", "1000-01-01 00:00:00"],
+                                           ["Data Scientist", "1000-01-01 00:00:00"],
+                                           ["Senior Data Analyst", "1000-01-01 00:00:00"],
+                                           ["Data Analyst", "1000-01-01 00:00:00"],
+                                           ["Graduate Data Analyst", "1000-01-01 00:00:00"],
+                                           ["Senior Data Engineer", "1000-01-01 00:00:00"],
+                                           ["Data Engineer", "1000-01-01 00:00:00"],
+                                           ["Senior Data Architect", "1000-01-01 00:00:00"],
+                                           ["Data Architect", "1000-01-01 00:00:00"],
+                                           ["Senior Business Intelligence Developer", "1000-01-01 00:00:00"],
+                                           ["Senior BI Developer", "1000-01-01 00:00:00"],
+                                           ["Senior Business Intelligence Specialist", "1000-01-01 00:00:00"],
+                                           ["Senior BI Specialist", "1000-01-01 00:00:00"],
+                                           ["Senior Business Intelligence Analyst", "1000-01-01 00:00:00"],
+                                           ["Senior BI Analyst", "1000-01-01 00:00:00"],
+                                           ["Business Intelligence Developer", "1000-01-01 00:00:00"],
+                                           ["BI Developer", "1000-01-01 00:00:00"],
+                                           ["Business Intelligence Specialist", "1000-01-01 00:00:00"],
+                                           ["BI Specialist", "1000-01-01 00:00:00"],
+                                           ["Business Intelligence Analyst", "1000-01-01 00:00:00"],
+                                           ["BI Analyst", "1000-01-01 00:00:00"],
+                                           ["Senior Machine Learning Engineer", "1000-01-01 00:00:00"],
+                                           ["Machine Learning Engineer", "1000-01-01 00:00:00"],
+                                           ["Data Visualization Developer", "1000-01-01 00:00:00"],
+                                           ["Senior ETL Developer", "1000-01-01 00:00:00"],
+                                           ["ETL Developer", "1000-01-01 00:00:00"],
+                                           ["Senior Database Developer", "1000-01-01 00:00:00"],
+                                           ["Database Developer", "1000-01-01 00:00:00"],
+                                           ["Senior Database Administrator", "1000-01-01 00:00:00"],
+                                           ["Database Administrator", "1000-01-01 00:00:00"],
+                                           ["Senior Cloud Developer", "1000-01-01 00:00:00"],
+                                           ["Senior Cloud Engineer", "1000-01-01 00:00:00"],
+                                           ["Senior Cloud Architect", "1000-01-01 00:00:00"],
+                                           ["Senior Cloud Consultant", "1000-01-01 00:00:00"],
+                                           ["Cloud Developer", "1000-01-01 00:00:00"],
+                                           ["Cloud Engineer", "1000-01-01 00:00:00"],
+                                           ["Cloud Architect", "1000-01-01 00:00:00"],
+                                           ["Cloud Consultant", "1000-01-01 00:00:00"]],
+                                           columns = ["search-string", "latest-timestamp"])
     jobSearchStrings = initializeVariables()
-    dataJobsDataframe, dataJobsMasterDataFrame, contactsList = loopOverRoles(latestDate = datetime(2020, 11, 6, 23, 59, 59, 99999),
+    dataJobsDataframe, dataJobsMasterDataFrame, contactsList = loopOverRoles(latestDate = datetime(2020, 11, 7, 23, 59, 59, 99999),
         scrapedJobs = scrapedJobs,
         scrapedJobsMaster = scrapedJobsMaster,
-        jobRoles = jobSearchStrings)
-    dataJobsDataframe.to_csv("Seek Data Jobs.csv", index = False)
-    dataJobsMasterDataFrame.to_csv("Seek Data Jobs Master.csv", index = False)
-    dataFrameJobContacts = pan.DataFrame(contactsList, columns = ["job-id", "contact-type", "value"])
-    dataFrameJobContacts.to_csv("Seek Data Jobs Contacts.csv", index = False)
+        jobRoles = jobSearchStrings,
+        latestTimestamps = scrapedJobsLatest)
+    if dataJobsDataframe.shape[0] > 0:
+        dataJobsDataframe.to_csv("Seek Data Jobs - " + datetime.now().strftime("%m%d%Y%H%M%S") + ".csv", index = False)
+        dataJobsMasterDataFrame.to_csv("Seek Data Jobs Master - " + datetime.now().strftime("%m%d%Y%H%M%S") + ".csv", index = False)
+        dataFrameJobContacts = pan.DataFrame(contactsList, columns = ["job-id", "contact-type", "value"])
+        dataFrameJobContacts.to_csv("Seek Data Jobs Contacts - " + datetime.now().strftime("%m%d%Y%H%M%S") + ".csv", index = False)
+    newScrapedJobsLatest = dataJobsDataframe.groupby(["search-string"], as_index = False).agg({"job-listing-date": "max"})
+    scrapedJobsLatest = scrapedJobsLatest.join(newScrapedJobsLatest.set_index("search-string"), on = ["search-string"], lsuffix = "-old", rsuffix = "-new")
+    scrapedJobsLatest["job-listing-date"] = scrapedJobsLatest["job-listing-date"].fillna("1000-01-01 00:00:00")
+    newScrapedJobsLatest = pan.DataFrame(columns = ["search-string", "latest-timestamp"])
+    newScrapedJobsLatest["search-string"] = scrapedJobsLatest["search-string"]
+    newScrapedJobsLatest["latest-timestamp"] = scrapedJobsLatest[["latest-timestamp", "job-listing-date"]].apply(lambda row: max([row[0], row[1]]), axis = 1)
+    newScrapedJobsLatest.to_csv("Scraped Jobs Latest Timestamps.csv", index = False)
